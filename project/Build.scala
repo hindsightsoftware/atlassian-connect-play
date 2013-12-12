b81fc0865c496f7b61a1ab55422b0e7c352a776c
Anders Holmgren <aholmgren@atlassian.com>
2013-12-12 16:59:12 +1100

    ACDEV-469 Added atlassian jwt deps

--- project/Build.scala
+++ project/Build.scala
@@ -6,6 +6,7 @@ object ApplicationBuild extends Build {
 
   val appName         = "ac-play-java"
   val appVersion      = "0.6.5-SNAPSHOT"
+  val atlassianJwtVersion = "1.0-m3"
 
   val appDependencies = Seq(
     "postgresql" % "postgresql" % "8.4-701.jdbc3",
@@ -16,6 +17,9 @@ object ApplicationBuild extends Build {
     "org.bouncycastle" % "bcprov-jdk16" % "1.46",
     "org.hibernate" % "hibernate-entitymanager" % "4.2.1.Final",
     "xml-apis" % "xml-apis" % "1.4.01",
+    "com.atlassian.jwt" % "jwt-api" % atlassianJwtVersion,
+    "com.atlassian.jwt" % "jwt-core" % atlassianJwtVersion,
+    "com.atlassian.jwt" % "jwt-http-client" % atlassianJwtVersion,
     javaCore,
     javaJdbc,
     javaJpa,
