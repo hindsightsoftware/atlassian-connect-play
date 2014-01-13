2e84d73e52c0853a25734ad8fd9cd7c84be1f57c
Anders Holmgren <aholmgren@atlassian.com>
2014-01-13 12:18:12 +1100

    ACDEV-469 added logging

--- app/com/atlassian/connect/play/java/auth/jwt/ACPlayJwtIssuerService.java
+++ app/com/atlassian/connect/play/java/auth/jwt/ACPlayJwtIssuerService.java
@@ -2,36 +2,42 @@ package com.atlassian.connect.play.java.auth.jwt;
 
 import com.atlassian.connect.play.java.AC;
 import com.atlassian.connect.play.java.AcHost;
+import com.atlassian.connect.play.java.util.Utils;
 import com.atlassian.jwt.core.reader.JwtIssuerSharedSecretService;
 import com.atlassian.jwt.core.reader.JwtIssuerValidator;
 import com.atlassian.jwt.exception.JwtIssuerLacksSharedSecretException;
 import com.atlassian.jwt.exception.JwtUnknownIssuerException;
 import com.google.common.base.Function;
+import com.google.common.base.Supplier;
+import play.Logger;
 
 public class ACPlayJwtIssuerService implements JwtIssuerSharedSecretService, JwtIssuerValidator {
+    private final static Logger.ALogger LOGGER = Utils.LOGGER;
 
     @Override
-    public String getSharedSecret(String issuer) throws JwtIssuerLacksSharedSecretException, JwtUnknownIssuerException {
+    public String getSharedSecret(final String issuer)  {
         return AC.getAcHost(issuer).map(new Function<AcHost, String>()
         {
             @Override
             public String apply(AcHost host)
             {
-                return host.getSharedSecret();
+                String sharedSecret = host.getSharedSecret();
+                if (sharedSecret == null) {
+                    LOGGER.warn("The issuer " + issuer + " does not have a shared secret in the database. Need to reinstall the addon to the host");
+                }
+                return sharedSecret;
             }
-        }).getOrNull();
+        }).getOrElse(new Supplier<String>() {
+            @Override
+            public String get() {
+                LOGGER.warn("The issuer " + issuer + " cannot be found in the database. Might be a host that has not registered");
+                return null;
+            }
+        });
     }
 
     @Override
     public boolean isValid(String issuer) {
-        try {
-            return getSharedSecret(issuer) != null;
-        } catch (JwtIssuerLacksSharedSecretException e) {
-            // TODO: log or something
-            return false;
-        } catch (JwtUnknownIssuerException e) {
-            // TODO: log or something
-            return false;
-        }
+        return getSharedSecret(issuer) != null;
     }
 }
