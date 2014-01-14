948896909f71eb1d06f69484b959694c889dc0d3
Anders Holmgren <aholmgren@atlassian.com>
2014-01-14 15:45:07 +1100

    ACDEV-469 added tests

--- test/com/atlassian/connect/play/java/auth/jwt/JwtAuthorizationGeneratorTest.java
+++ test/com/atlassian/connect/play/java/auth/jwt/JwtAuthorizationGeneratorTest.java
@@ -1,21 +1,17 @@
 package com.atlassian.connect.play.java.auth.jwt;
 
 import com.atlassian.connect.play.java.AC;
-import com.atlassian.connect.play.java.AcHost;
-import com.atlassian.connect.play.java.http.HttpMethod;
 import com.atlassian.connect.play.java.model.AcHostModel;
 import com.atlassian.fugue.Option;
 import com.atlassian.jwt.SigningAlgorithm;
-import com.atlassian.jwt.core.writer.NimbusJwtWriterFactory;
+import com.atlassian.jwt.core.TimeUtil;
 import com.atlassian.jwt.exception.JwtIssuerLacksSharedSecretException;
 import com.atlassian.jwt.exception.JwtUnknownIssuerException;
 import com.atlassian.jwt.writer.JwtWriter;
 import com.atlassian.jwt.writer.JwtWriterFactory;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.google.common.collect.ImmutableMap;
-import org.apache.commons.lang.ObjectUtils;
 import org.hamcrest.Matcher;
-import org.json.JSONObject;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -26,12 +22,11 @@ import play.libs.Json;
 import play.test.FakeApplication;
 import play.test.Helpers;
 
-import java.net.URI;
 import java.net.URISyntaxException;
 import java.util.List;
 
 import static com.atlassian.jwt.JwtConstants.HttpRequests.JWT_AUTH_HEADER_PREFIX;
-import static org.hamcrest.CoreMatchers.startsWith;
+import static org.hamcrest.Matchers.*;
 import static org.junit.Assert.assertThat;
 import static org.mockito.Matchers.any;
 import static org.mockito.Matchers.anyString;
@@ -89,7 +84,23 @@ public class JwtAuthorizationGeneratorTest {
         verify(jwtWriter).jsonToJwt(argThat(isJwtWithSubValue(FREDDY.get())));
     }
 
-    // TODO: tests for exp and iat
+    @Test
+    public void callsWriterWithIatCloseToNow() throws JwtUnknownIssuerException, URISyntaxException, JwtIssuerLacksSharedSecretException {
+        generate();
+        verify(jwtWriter).jsonToJwt(argThat(isJwtWithIatValueCloseToNow()));
+    }
+
+    @Test
+    public void callsWriterWithExpValueGreaterThanOrEqualToNow() throws JwtUnknownIssuerException, URISyntaxException, JwtIssuerLacksSharedSecretException {
+        generate();
+        verify(jwtWriter).jsonToJwt(argThat(isJwtWithExpValueGreaterThanOrEqualToNow()));
+    }
+
+    @Test
+    public void callsWriterWithCorrectQueryHash() throws JwtUnknownIssuerException, JwtIssuerLacksSharedSecretException, URISyntaxException {
+        generate();
+        verify(jwtWriter).jsonToJwt(argThat(isJwtWithStringFieldValue("qsh", "dc884e24fe0f4113b128fd19b1426d7d841b6fabc03e79c2d4f27774964a5935")));
+    }
 
     private Option<String> generate() throws JwtIssuerLacksSharedSecretException, JwtUnknownIssuerException, URISyntaxException {
         return jwtAuthorizationGenerator.generate("GET", aUrl, ImmutableMap.<String, List<String>>of(), acHost, FREDDY);
@@ -103,6 +114,14 @@ public class JwtAuthorizationGeneratorTest {
         return isJwtWithStringFieldValue("sub", subject);
     }
 
+    private Matcher<String> isJwtWithIatValueCloseToNow() {
+        return isJwtWithLongValueMatching("iat", lessThanOrEqualTo(TimeUtil.currentTimeSeconds()));
+    }
+
+    private Matcher<String> isJwtWithExpValueGreaterThanOrEqualToNow() {
+        return isJwtWithLongValueMatching("exp", greaterThanOrEqualTo(TimeUtil.currentTimeSeconds()));
+    }
+
     private Matcher<String> isJwtWithStringFieldValue(String fieldName, String fieldValue) {
         return new JwtJsonMatcher<String>(fieldName, fieldValue) {
             @Override
@@ -112,26 +131,35 @@ public class JwtAuthorizationGeneratorTest {
         };
     }
 
+    private Matcher<String> isJwtWithLongValueMatching(String fieldName, Matcher<Long> fieldMatcher) {
+        return new JwtJsonMatcher<Long>(fieldName, fieldMatcher) {
+            @Override
+            Long getValue(JsonNode node) {
+                return node.longValue();
+            }
+        };
+    }
+
     private abstract class JwtJsonMatcher<T> extends ArgumentMatcher<String> {
         private final String fieldName;
-        private final T fieldValue;
+        private final Matcher<T> fieldMatcher;
 
         public JwtJsonMatcher(String fieldName, T fieldValue) {
+            this(fieldName, equalTo(fieldValue));
+        }
+
+        public JwtJsonMatcher(String fieldName, Matcher<T> fieldMatcher) {
             this.fieldName = fieldName;
-            this.fieldValue = fieldValue;
+            this.fieldMatcher = fieldMatcher;
         }
 
         @Override
         public boolean matches(Object argument) {
-            System.out.println(argument);
             JsonNode jwtJson = Json.parse(argument.toString());
 
             JsonNode jwtFieldJson = jwtJson.get(fieldName);
-            if (jwtFieldJson == null)
-                return fieldValue == null;
-
-            T value = getValue(jwtFieldJson);
-            return ObjectUtils.equals(value, fieldValue);
+            T value = jwtFieldJson == null ? null : getValue(jwtFieldJson);
+            return fieldMatcher.matches(value);
         }
 
         abstract T getValue(JsonNode node);
