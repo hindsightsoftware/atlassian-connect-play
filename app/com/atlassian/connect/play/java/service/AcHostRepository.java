61dbb463cd97b26fe7befdd8e900736f3c9f2052
Alan Parkinson <alan.parkinson@hindsightsoftware.co.uk>
2014-06-21 19:54:31 +0100

    AC-825 Added Javadoc to the Repository Interface

--- app/com/atlassian/connect/play/java/service/AcHostRepository.java
+++ app/com/atlassian/connect/play/java/service/AcHostRepository.java
@@ -7,11 +7,46 @@ import java.util.List;
 
 public interface AcHostRepository {
 
-    // Throwable cause that's what play throws!!!!
+    /**
+     * Saves a AC Host to the repository
+     * @param acHost
+     * @throws Throwable
+     */
     void save(AcHost acHost) throws Throwable;
+
+    /**
+     * Retrieves a list of all the AC Host entries stored in the repository
+     * @return
+     * @throws Throwable
+     */
     List<? extends AcHost> all() throws Throwable;
+
+    /**
+     * Retrieves a AC Host entry from the repository
+     * @param key The unique key of the AC Host to retrieve
+     * @return
+     * @throws Throwable
+     */
     Option<AcHost> findByKey(String key) throws Throwable;
+
+    /**
+     * Retrieves a AC Host entry from the repository
+     * @param baseUrl The url of the AC Host to retrieve
+     * @return
+     * @throws Throwable
+     */
     Option<AcHost> findByUrl(String baseUrl) throws Throwable;
+
+    /**
+     * Deletes a AC Host from the repository
+     * @param id
+     * @throws Throwable
+     */
     void delete(Long id) throws Throwable;
+
+    /**
+     * Factory method to create a new empty instance of ACHost. This instance will not be stored in the Repository.
+     * @return
+     */
     AcHost create();
 }
