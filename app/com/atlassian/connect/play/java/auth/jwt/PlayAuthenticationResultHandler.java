2cdae05b90ac3377e6830dc8c588f492dc7012e5
Alan Parkinson <alan.parkinson@hindsightsoftware.co.uk>
2014-08-11 18:19:48 +0100

    AC-1288 Use the preconfigured 'ac' Play logger from Logback

--- app/com/atlassian/connect/play/java/auth/jwt/PlayAuthenticationResultHandler.java
+++ app/com/atlassian/connect/play/java/auth/jwt/PlayAuthenticationResultHandler.java
@@ -2,18 +2,15 @@ package com.atlassian.connect.play.java.auth.jwt;
 
 import com.atlassian.jwt.Jwt;
 import com.atlassian.jwt.core.http.auth.AuthenticationResultHandler;
-import com.atlassian.jwt.core.http.auth.JwtAuthenticator;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 import java.security.Principal;
 
+import static com.atlassian.connect.play.java.util.Utils.LOGGER;
 import static play.mvc.Http.Response;
 import static play.mvc.Http.Status.*;
 import static play.mvc.Results.status;
 
 public class PlayAuthenticationResultHandler implements AuthenticationResultHandler<Response, JwtAuthenticationResult> {
-    private static final Logger log = LoggerFactory.getLogger(PlayAuthenticationResultHandler.class);
 
     @Override
     public JwtAuthenticationResult createAndSendInternalError(Exception e, Response response, String externallyVisibleMessage)
@@ -51,13 +48,13 @@ public class PlayAuthenticationResultHandler implements AuthenticationResultHand
 
     private static JwtAuthenticationResult createError(Exception e, int httpResponseCode, String externallyVisibleMessage)
     {
-        log.debug("Error during JWT authentication: ", e);
+        LOGGER.debug("Error during JWT authentication: ", e);
         return createErrorResult(httpResponseCode, externallyVisibleMessage);
     }
 
     private static JwtAuthenticationResult createFailure(Exception e, int httpResponseCode, String externallyVisibleMessage)
     {
-        log.debug("Failure during JWT authentication: ", e);
+        LOGGER.debug("Failure during JWT authentication: ", e);
         return createErrorResult(httpResponseCode, externallyVisibleMessage);
     }
 
