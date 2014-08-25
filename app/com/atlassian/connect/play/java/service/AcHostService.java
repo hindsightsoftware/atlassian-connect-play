e4fec06b62810f85df8deccb64c94e5bccf79a3e
Anders Holmgren <aholmgren@atlassian.com>
2014-08-25 10:57:38 +1000

    update version

--- app/com/atlassian/connect/play/java/service/AcHostService.java
+++ app/com/atlassian/connect/play/java/service/AcHostService.java
@@ -25,9 +25,6 @@ public interface AcHostService {
      * Registers a new host, or updates the details from an existing host
      *
      *
-     *
-     *
-     *
      * @param clientKey
      * @param  baseUrl
      * @param publicKey
